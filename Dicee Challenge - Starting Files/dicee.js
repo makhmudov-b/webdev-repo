73f65d4a36ef76fc88ca79ff953cdc3aa8c19a98
Bekjon Makhmudov <makhmudov_b@auca.kg>
2021-02-22 23:44:49 +0500

    new

---
+++ Dicee Challenge - Starting Files/dicee.js
@@ -0,0 +1,4 @@
+alert("Hello");
+var ran = Math.floor(Math.random() * 6) + 1;
+var links=["images\dice6.png","images\dice5.png","images\dice4.png","images\dice3.png","images\dice2.png","images\dice1.png"];
+document.querySelector(".container").src('links[4]');
