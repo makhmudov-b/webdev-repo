fd5b5fb6d6cdfc72c898f85f4494a975cb5f7484
Bekjon Makhmudov <makhmudov_b@auca.kg>
2021-02-25 21:43:55 +0500

    dice game ready

--- Dicee Challenge - Starting Files/dicee.js
+++ Dicee Challenge - Starting Files/dicee.js
@@ -1,4 +1,17 @@
-alert("Hello");
-var ran = Math.floor(Math.random() * 6) + 1;
-var links=["images\dice6.png","images\dice5.png","images\dice4.png","images\dice3.png","images\dice2.png","images\dice1.png"];
-document.querySelector(".container").src('links[4]');
+var pics = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
+
+var ran = Math.floor(Math.random() * 6);
+document.querySelector("img.img1").setAttribute("src", pics[ran]);
+
+var ran2 = Math.floor(Math.random() * 6);
+document.querySelector("img.img2").setAttribute("src", pics[ran2]);
+
+if (ran > ran2) {
+    document.querySelector(".container h1").innerHTML = "Player 1 Wins!";
+}
+else if (ran < ran2) {
+    document.querySelector(".container h1").innerHTML = "Player 2 Wins!";
+}
+else {
+    document.querySelector(".container h1").innerHTML = "Draw!";
+}
