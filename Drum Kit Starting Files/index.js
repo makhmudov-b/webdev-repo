33304f7abd93c908b4cf4014fd2e40d92eb807e0
Bekjon Makhmudov <makhmudov_b@auca.kg>
2021-02-26 19:36:56 +0500

    new one starting

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,7 @@
+for (var index = 0; index < document.querySelectorAll(".drum").length; index++) {
+    document.querySelectorAll("button")[index].addEventListener("click", function ClickOn() {
+        alert("It works!");
+    });
+}
+
+
