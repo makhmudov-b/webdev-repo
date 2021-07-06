3c6c62f0854c58e0799133e1d801697a5d444141
Bekjon Makhmudov <makhmudov_b@auca.kg>
2021-07-06 19:51:17 +0600

    new

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -3,16 +3,21 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
-}); 
-
-app.post("/", function(req, res){
-    res.send("It works!");
+    res.sendFile(__dirname + "/bmiCalculator.html");
 });
 
-app.listen(3000, function(){
+app.post("/", function (req, res) {
+    var num1 = parseFloat(req.body.height);
+    var num2 = parseFloat(req.body.weight);
+
+    var result = num2 / (num1 * num1);
+
+    res.send("The answer is " + result);
+});
+
+app.listen(3000, function () {
     console.log("Server is working on port 3000.");
 });
