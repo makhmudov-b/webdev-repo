4ef0765c093f178dde781a390b678f3e39fecc63
Bekjon Makhmudov <makhmudov_b@auca.kg>
2021-07-15 22:42:11 +0600

    new server

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -4,6 +4,18 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("static"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+})
+app.post("/", function (req, res) {
+    console.log(req.body.Name);
+    
+})
 app.listen(3000, function () {
     console.log("Localhost 3000 is working....");
 })
+// a88756f157e7362040ca1fb5543e197a-us6
+// 9277704633
