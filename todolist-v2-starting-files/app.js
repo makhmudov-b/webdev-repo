198992b3e3a84d97b1ece7b7ab849641a3e8e2ef
Bekjon Makhmudov <makhmudov_b@auca.kg>
2021-09-10 00:17:00 +0600

    Challenge 3

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://admin-bekjon:besiktas1903@cluster0.m4ucl.mongodb.net/todolistDB", { useNewUrlParser: true });
 const itemsScema = {
   name: String
 };
@@ -127,6 +127,12 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
