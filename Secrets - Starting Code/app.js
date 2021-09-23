7b7f2b8dd628e9bff3514afef0701da500285e47
Bekjon Makhmudov <makhmudov_b@auca.kg>
2021-09-24 01:35:36 +0600

    security

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -1,1 +1,73 @@
 //jshint esversion:6
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const md5 = require("md5");
+
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const userScema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = mongoose.model("User", userScema);
+
+app.get("/", function (req, res) {
+    res.render('home');
+});
+
+
+app.get("/login", function (req, res) {
+    res.render('login');
+});
+
+app.get("/register", function (req, res) {
+    res.render('register');
+});
+
+app.post("/register", function (req, res) {
+    const newuser = new User({
+        email: req.body.username,
+        password: md5(req.body.password)
+    });
+    newuser.save(function (err) {
+        if (!err) {
+            res.render("secrets");
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.post("/login", function (req, res) {
+    const userName = req.body.username;
+    const password = md5(req.body.password);
+
+    User.findOne({ email: userName }, function (err, foundOne) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundOne) {
+                if (foundOne.password === password) {
+                    res.render("secrets");
+                }
+            }
+        }
+    })
+});
+
+app.listen(3000, function () {
+    console.log("The server is running on port 3000...");
+});
+
